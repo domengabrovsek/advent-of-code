17fcb8d0830f250017ffcbb4eee4381209195b46
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-12-04 21:14:31 +0100

    Finished day 4

---
+++ src/day-4/tests/day-4.test.js
@@ -0,0 +1,25 @@
+const Day4 = require('../index');
+
+describe('tests for day 4 challenge', () => {
+
+  const testCases = [
+    { file: 'input.txt', expectedResult1: 82440, expectedResult2: 20774 },
+    { file: 'input-test.txt', expectedResult1: 4512, expectedResult2: 1924 },
+  ]
+
+  testCases.forEach(tc => {
+    test(`input: ${tc.file}`, () => {
+
+      // arrange
+      const inputFile = `${__dirname}/${tc.file}`;
+
+      // act 
+      const result1 = new Day4(inputFile).part1(1);
+      const result2 = new Day4(inputFile).part1(2);
+
+      // assert
+      expect(result1).toBe(tc.expectedResult1);
+      expect(result2).toBe(tc.expectedResult2);
+    })
+  })
+});
--- utils.js
+++ utils.js
@@ -1,13 +1,3 @@
-const fs = require('fs');
-
-export const readFile = (file) =>
-  fs.readFileSync(file, { encoding: 'utf8' })
-    .split('\n')
-    .map(row => row.replace('\r', ''));
-
-export const sumItemsInArray = (array) => array.reduce((sum, item) => sum + item, 0);
-
-export const multiplyItemsInArray = (array) => array.reduce((product, item) => product * item, 1);
-
-export const countCharsInString = (string, char) => string.split('').filter(x => x === char).length;
-
+module.exports = {
+  deepCopy: (obj) => obj && JSON.parse(JSON.stringify(obj))
+};
