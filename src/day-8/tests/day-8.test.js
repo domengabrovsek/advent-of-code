e4278b173128c4ea1833fdd1e121184182451d86
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-12-08 22:31:29 +0100

    Finished day 8

--- src/day-8/tests/day-8.test.js
+++ src/day-8/tests/day-8.test.js
@@ -1,10 +1,10 @@
-const Day7 = require('../index');
+const Day8 = require('../index');
 
-describe('tests for day 7 challenge', () => {
+describe('tests for day 8 challenge', () => {
 
   const testCases = [
-    { file: 'input.txt', expectedResult1: 534, expectedResult2: 100148777 },
-    { file: 'input-test.txt', expectedResult1: 26, expectedResult2: 168 },
+    { file: 'input.txt', expectedResult1: 534, expectedResult2: 1070188 },
+    { file: 'input-test.txt', expectedResult1: 26, expectedResult2: 61229 },
   ]
 
   testCases.forEach(tc => {
@@ -14,12 +14,12 @@ describe('tests for day 7 challenge', () => {
       const inputFile = `${__dirname}/${tc.file}`;
 
       // act 
-      const result1 = new Day7(inputFile).solve(1);
-      // const result2 = new Day7(inputFile).solve(2);
+      const result1 = new Day8(inputFile).solve(1);
+      const result2 = new Day8(inputFile).solve(2);
 
       // assert
       expect(result1).toBe(tc.expectedResult1);
-      // expect(result2).toBe(tc.expectedResult2);
+      expect(result2).toBe(tc.expectedResult2);
     })
   })
 });
