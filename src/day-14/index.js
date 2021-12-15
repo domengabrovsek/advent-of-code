0c0ec8a2ff13fc550804123b067e66b26db91660
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-12-15 19:14:45 +0100

    Finished day 14 with a bit smarter implementation

--- src/day-14/index.js
+++ src/day-14/index.js
@@ -14,61 +14,97 @@ module.exports = class Day14 {
 
     this.template = rows.shift();
     this.dict = Object.fromEntries(rows.map(row => row.split(' -> ')));
-
   }
 
-  convert(template) {
+  getMostLeastCommonElement(pairs) {
 
-    let newTemplate = '';
+    // get a list of unique elements
+    const uniqueElements = [...new Set(Object.keys(pairs).flatMap(x => x.split('')))];
 
-    for (let j = 0; j < template.length - 1; j++) {
-      const first = template?.[j];
-      const second = template?.[j + 1];
-      const third = this.dict[`${first}${second}`];
-      const joined = `${first}${third}`;
+    let elements = {};
 
-      if (first && second) {
-        newTemplate += joined;
-      }
+    uniqueElements.forEach(x => {
+      elements[x] = 0;
+    })
 
-      if (j === template.length - 2) {
-        newTemplate += second;
+    // count number of specific element
+    for (const pair of Object.keys(pairs)) {
+      for (const element of pair.split('')) {
+        elements[element] += pairs[pair] / 2;
       }
     }
 
-    return newTemplate;
-  }
+    // round them up in case of floats
+    for (const x of Object.keys(elements)) {
+      elements[x] = Math.round(elements[x])
+    }
 
-  getMostLeastCommonElement(template) {
-    const uniqueElements = [...new Set(template)];
-    const elements = uniqueElements.map(element => ({
-      key: element,
-      value: template.split('').filter(x => x === element).length
-    }));
-
-    const mostCommon = Math.max(...elements.map(x => x.value));
-    const leastCommon = Math.min(...elements.map(x => x.value));
-
-    // const mostCommon = elements.find(x => x.value === max);
-    // const leastCommon = elements.find(x => x.value === min);
+    // get most and least common elements
+    const mostCommon = Math.max(...Object.keys(elements).map(x => elements[x]));
+    const leastCommon = Math.min(...Object.keys(elements).map(x => elements[x]));
 
     return { mostCommon, leastCommon };
   }
 
-  solve(part) {
+  getNewPairs(pairs, mapInstructions) {
+    // we need a deep copy
+    const newPairs = JSON.parse(JSON.stringify(pairs));
 
+    for (const key of Object.keys(mapInstructions)) {
+      const value = pairs[key];
 
-    let steps = part === 1 ? 10 : 40;
-    let template = this.template;
-
-    for (let i = 0; i < steps; i++) {
-      template = this.convert(template);
+      if (value > 0) {
+        newPairs[key] = (newPairs[key] || 0) - value;
+        newPairs[mapInstructions[key][0]] = (newPairs[mapInstructions[key][0]] || 0) + value;
+        newPairs[mapInstructions[key][1]] = (newPairs[mapInstructions[key][1]] || 0) + value;
+      }
     }
 
-    const { mostCommon, leastCommon } = this.getMostLeastCommonElement(template);
+    return newPairs;
+  }
+
+  getInitialPairs(template) {
+    let pairs = {};
+
+    for (let i = 0; i < template.length - 1; i++) {
+      const pair = template.substr(i, 2);
+      pairs[pair] = pairs?.[pair] || 0 + 1;
+    }
+
+    return pairs;
+  }
+
+  getInitialMapInstructions(dict) {
+    const mapInstructions = {};
+
+    for (const key of Object.keys(dict)) {
+      const [first, second] = key.split('');
+      const third = dict[key];
+      mapInstructions[key] = [`${first}${third}`, `${third}${second}`];
+    }
+
+    return mapInstructions;
+  }
+
+  solve(part) {
+
+    // initial pairs from input
+    let pairs = this.getInitialPairs(this.template);
+
+    // initial map instructions from input
+    const mapInstructions = this.getInitialMapInstructions(this.dict);
+
+    // part 1 has 10 steps
+    // part 2 has 40 steps
+    let steps = part === 1 ? 10 : 40;
+
+    for (let i = 0; i < steps; i++) {
+      pairs = this.getNewPairs(pairs, mapInstructions);
+    }
+
+    // get most and least common elements
+    const { mostCommon, leastCommon } = this.getMostLeastCommonElement(pairs);
 
     return mostCommon - leastCommon;
-
   }
 }
-
