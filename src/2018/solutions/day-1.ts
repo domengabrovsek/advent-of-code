7d388d686978329018df70c47bbbf07134503d59
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-11-28 19:06:00 +0100

    part 1 2018

--- src/2018/solutions/day-1.ts
+++ src/2018/solutions/day-1.ts
@@ -7,7 +7,13 @@ export const solveOne = async () => {
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
 
-  // TODO: Implement part one
+  // parse input to get numbers
+  const numbers = input.split('\n').map(row => row.includes('+') ? Number(row.split('+')[1]) : Number(row));
+
+  // sum number
+  const sum = numbers.reduce((sum, curr) => sum + curr, 0);
+
+  return sum;
 }
 
 export const solveTwo = async () => {
