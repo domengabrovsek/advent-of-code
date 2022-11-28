9f12e0122524eddc9bd0c0494430981e9d4dcfa6
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-11-28 19:41:11 +0100

    autogenerated 2022

---
+++ src/2022/solutions/day-19.ts
@@ -0,0 +1,19 @@
+/* This file contains solution for AoC puzzle day 19 */
+
+import { getYear, getDay, getInput } from '../../utils/utils';
+
+export const solveOne = async () => {
+
+  // raw input
+  const input = await getInput(getYear(__filename), getDay(__filename));
+
+  // TODO: Implement part one
+}
+
+export const solveTwo = async () => {
+
+  // raw input
+  const input = await getInput(getYear(__filename), getDay(__filename));
+
+  // TODO: Implement part two
+}
---
+++ src/2022/solutions/day-5.ts
@@ -0,0 +1,19 @@
+/* This file contains solution for AoC puzzle day 5 */
+
+import { getYear, getDay, getInput } from '../../utils/utils';
+
+export const solveOne = async () => {
+
+  // raw input
+  const input = await getInput(getYear(__filename), getDay(__filename));
+
+  // TODO: Implement part one
+}
+
+export const solveTwo = async () => {
+
+  // raw input
+  const input = await getInput(getYear(__filename), getDay(__filename));
+
+  // TODO: Implement part two
+}
