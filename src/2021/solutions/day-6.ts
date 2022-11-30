98bb53cc7b94717cb81cfbd9647080296b13413c
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-11-30 15:55:46 +0100

    day 6 refactor

--- src/2021/solutions/day-6.ts
+++ src/2021/solutions/day-6.ts
@@ -2,12 +2,53 @@
 
 import { getYear, getDay, getInput } from '../../utils/utils';
 
+const solve = (input: any, days: number) => {
+  const fishArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  input
+    .split(',')
+    .filter(Boolean)
+    .map((x: any) => parseInt(x)).forEach((fish: any) => {
+      if (!fishArray[fish]) {
+        fishArray[fish] = 1;
+      } else {
+        fishArray[fish] += 1;
+      }
+    });
+
+  // simulate days
+  for (let i = 1; i <= days; i++) {
+
+    const newFish = fishArray[0];
+
+    // remove level 0 fish
+    if (fishArray[0]) {
+      fishArray[0] = 0;
+    }
+
+    // simulate fish reproduction
+    for (let j = 1; j < fishArray.length; j++) {
+      if (fishArray[j]) {
+        fishArray[j - 1] += fishArray[j];
+        fishArray[j] -= fishArray[j];
+      }
+    }
+
+    // add newly born and reset fish
+    fishArray[8] += newFish;
+    fishArray[6] += newFish;
+  }
+
+  // sum all the fish
+  return fishArray.reduce((x, y) => x + y, 0);
+}
+
 export const solveOne = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
 
-  // TODO: Implement part one
+  return solve(input, 80)
 }
 
 export const solveTwo = async () => {
@@ -15,5 +56,5 @@ export const solveTwo = async () => {
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
 
-  // TODO: Implement part two
+  return solve(input, 256)
 }
