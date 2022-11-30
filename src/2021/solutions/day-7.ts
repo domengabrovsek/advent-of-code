e9a07e4c3bc67c329f63e92db333711cb223e2c0
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-11-30 16:39:10 +0100

    day 7 refactor

--- src/2021/solutions/day-7.ts
+++ src/2021/solutions/day-7.ts
@@ -2,18 +2,56 @@
 
 import { getYear, getDay, getInput } from '../../utils/utils';
 
+const stepCost = (position: any, number: any, part: any) => {
+  const cost = Math.abs(position - number);
+
+  if (part === 1) { return cost; }
+
+  let sum = 0;
+
+  for (let i = 0; i < cost; i++) {
+    sum += i;
+  }
+
+  return cost + sum;
+}
+
+const getPositionsToCheck = (input: any) => {
+  const inputNumbers: any = Array.from(new Set(input));
+  const max = Math.max(...inputNumbers);
+  const min = Math.min(...inputNumbers);
+
+  const numbers = [];
+  for (let i = min; i <= max; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+
 export const solveOne = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const parsedInput = input.split(',').map(Number);
 
-  // TODO: Implement part one
+  return Math
+    .min(...getPositionsToCheck(parsedInput)
+      .map(position => parsedInput
+        .map(crab => stepCost(position, crab, 1))
+        .reduce((x, y) => x + y, 0))
+    );
 }
 
 export const solveTwo = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const parsedInput = input.split(',').map(Number);
 
-  // TODO: Implement part two
+  return Math
+    .min(...getPositionsToCheck(parsedInput)
+      .map(position => parsedInput
+        .map(crab => stepCost(position, crab, 2))
+        .reduce((x, y) => x + y, 0))
+    );
 }
