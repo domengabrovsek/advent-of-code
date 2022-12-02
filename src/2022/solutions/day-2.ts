070002c5b169b10a00f34d7b475ca6445a9bfaef
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-12-02 06:35:18 +0100

    2022 2

--- src/2022/solutions/day-2.ts
+++ src/2022/solutions/day-2.ts
@@ -2,18 +2,60 @@
 
 import { getYear, getDay, getInput } from '../../utils/utils';
 
+// A X 1 ROCK
+// B Y 2 PAPER
+// C Z 3 SCISSOR
+const score: any = {
+  'AY': 8,
+  'AX': 4,
+  'AZ': 3,
+  'BY': 5,
+  'BX': 1,
+  'BZ': 9,
+  'CY': 2,
+  'CX': 7,
+  'CZ': 6,
+}
+
+// X LOSE
+// Y DRAW
+// Z WIN
+const nextMove: any = {
+  'AY': 'X',
+  'AX': 'Z',
+  'AZ': 'Y',
+  'BY': 'Y',
+  'BX': 'X',
+  'BZ': 'Z',
+  'CY': 'Z',
+  'CX': 'Y',
+  'CZ': 'X',
+}
+
 export const solveOne = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const parsedInput = input.split('\n');
 
-  // TODO: Implement part one
+  let sum = 0;
+  parsedInput.forEach(row => sum += score[row.split(' ').join('')]);
+  return sum;
 }
 
 export const solveTwo = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const parsedInput = input.split('\n');
 
-  // TODO: Implement part two
+  let sum = 0;
+
+  parsedInput.forEach(row => {
+    const myMove = nextMove[row.split(' ').join('')];
+    const play = `${row[0]}${myMove}`
+    sum += score[play];
+  })
+
+  return sum;
 }
