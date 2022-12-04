171e5d14425702bffd498a44fee768d755fd8b85
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-12-04 13:30:26 +0100

    Day 4

--- src/2022/solutions/day-4.ts
+++ src/2022/solutions/day-4.ts
@@ -6,14 +6,36 @@ export const solveOne = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const parsedInput = input.split('\n');
 
-  // TODO: Implement part one
+  let overlaps = 0;
+
+  for (let row of parsedInput) {
+    let [a, b, c, d] = row.split(',').flatMap(x => x.split('-').map(x => parseInt(x)));
+
+    if ((a >= c && b <= d) || (c >= a && d <= b)) {
+      overlaps += 1;
+    }
+  }
+
+  return overlaps;
 }
 
 export const solveTwo = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const parsedInput = input.split('\n');
 
-  // TODO: Implement part two
+  let overlaps = 0;
+
+  for (let row of parsedInput) {
+    let [a, b, c, d] = row.split(',').flatMap(x => x.split('-').map(x => parseInt(x)));
+
+    if ((a >= c && a <= d) || (b >= c && b <= d) || (c >= a && c <= b) || (d >= a && d <= b)) {
+      overlaps += 1;
+    }
+  }
+
+  return overlaps;
 }
