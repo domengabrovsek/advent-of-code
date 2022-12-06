62dae822eff0b02af67d42cf32fca89171959928
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-12-06 10:20:17 +0100

    6 smaller

--- src/2022/solutions/day-6.ts
+++ src/2022/solutions/day-6.ts
@@ -2,19 +2,15 @@
 
 import { getYear, getDay, getInput } from '../../utils/utils';
 
-export const solveOne = async () => {
-
-  // raw input
-  const input = await getInput(getYear(__filename), getDay(__filename));
-
+const solve = (input: any, size: number) => {
   let packet: string[] = [];
 
   for (let i = 0; i < input.length; i++) {
     const char = input[i];
 
-    if (packet.length === 4) {
-    
-      if (new Set(packet).size === 4) {
+    if (packet.length === size) {
+
+      if (new Set(packet).size === size) {
         return i;
       }
 
@@ -24,25 +20,16 @@ export const solveOne = async () => {
   }
 }
 
+export const solveOne = async () => {
+
+  // raw input
+  const input = await getInput(getYear(__filename), getDay(__filename));
+  return solve(input, 4);
+}
+
 export const solveTwo = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
-
-  let packet: string[] = [];
-
-  for (let i = 0; i < input.length; i++) {
-    const char = input[i];
-
-    if (packet.length === 14) {
-    
-      if (new Set(packet).size === 14) {
-        return i;
-      }
-
-      packet.shift();
-    }
-
-    packet.push(char);
-  }
+  return solve(input, 14);
 }
