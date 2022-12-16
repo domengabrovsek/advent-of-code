dc1b945ea6511452e1ea92606d851b06912470ba
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-12-16 18:34:36 +0100

    Day 7

--- src/2022/solutions/day-7.ts
+++ src/2022/solutions/day-7.ts
@@ -2,18 +2,54 @@
 
 import { getYear, getDay, getInput } from '../../utils/utils';
 
+const getDirectories = (input: string) => {
+  const lines = input.split('\n')
+    .filter(line => !line.startsWith('$ ls'))
+    .filter(line => !line.startsWith('dir'))
+
+  const directories: Record<string, number> = {}
+  const current: string[] = [];
+
+  for (let line of lines) {
+
+    const [a, b, c] = line.split(' ');
+
+    if (a === '$') {
+      if (c === '..') {
+        current.pop();
+      } else {
+        current.push(c);
+      }
+    } else {
+      const pathArray = [...current];
+      while (pathArray.length) {
+        const currentPath = pathArray.join('/');
+        directories[currentPath] = (directories[currentPath] || 0) + Number(a);
+        pathArray.pop();
+      }
+    }
+  }
+
+  return directories;
+}
+
+
 export const solveOne = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const directories = getDirectories(input);
 
-  // TODO: Implement part one
+  return Array.from(Object.values(directories)).filter(sum => sum <= 100000).reduce((a, b) => a + b, 0);
 }
 
 export const solveTwo = async () => {
 
   // raw input
   const input = await getInput(getYear(__filename), getDay(__filename));
+  const directories = getDirectories(input);
 
-  // TODO: Implement part two
+  return Array.from(Object.values(directories))
+    .filter(sum => sum >= 30000000 - (70000000 - directories['/']))
+    .sort()[0];
 }
