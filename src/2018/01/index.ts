bcdf8e5ca77070e7fded04e78eec1e53a1f28075
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2023-11-30 18:52:07 +0100

    refactor 2018

--- src/2018/01/index.ts
+++ src/2018/01/index.ts
@@ -1,12 +1,6 @@
-/* This file contains solution for AoC puzzle day 1 */
 
-import { getYear, getDay, getInput } from '../../utils/utils';
 
-export const solveOne = async () => {
-
-  // raw input
-  const input = await getInput(getYear(__filename), getDay(__filename));
-
+export const solveOne = (input: string) => {
   // parse input to get numbers
   const numbers = input.split('\n').map(row => row.includes('+') ? Number(row.split('+')[1]) : Number(row));
 
@@ -16,11 +10,7 @@ export const solveOne = async () => {
   return sum;
 }
 
-export const solveTwo = async () => {
-
-  // raw input
-  const input = await getInput(getYear(__filename), getDay(__filename));
-
+export const solveTwo = (input: string) => {
   // parse input to get numbers
   const numbers = input.split('\n')
     .filter(Boolean)
